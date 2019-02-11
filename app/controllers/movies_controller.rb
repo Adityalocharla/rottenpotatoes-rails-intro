84186c73e11718737ed6355a8aa800532102d0c8
EC2 Default User <ec2-user@ip-172-31-34-248.us-east-2.compute.internal>
2019-02-11 02:04:05 +0000

    par1 test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,13 +13,16 @@ class MoviesController < ApplicationController
   def index
     sort = params[:sort]
     @movies = Movie.all
+    @title_header = []
+    @release_date_header = []
+    @release_date_header = 
     @all_ratings = ['G','PG','PG-13','R']
     if sort == 'title'
       @movies = Movie.all.sort_by { |h | h[:title] }
-      @title_header = [hilite]
+      @title_header = ['hilite']
     elsif sort == 'release_date'
       @movies = Movie.all.sort_by { |h | h[:release_date] }
-      @release_date_header = [hilite]
+      @release_date_header = ['hilite']
     end
       
   end
