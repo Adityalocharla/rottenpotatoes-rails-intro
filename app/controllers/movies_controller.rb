78e9ebb0e5a5a7d1388f78eaf33d4a2c76193ee3
EC2 Default User <ec2-user@ip-172-31-34-248.us-east-2.compute.internal>
2019-02-11 03:06:29 +0000

    par1 test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,17 +12,8 @@ class MoviesController < ApplicationController
 
   def index
     sort = params[:sort]
-    @movies = Movie.all
-    @title_header = []
-    @release_date_header = []
+    @movies = Movie.with_rating_sorting(@filters, sort)
     @all_ratings = ['G','PG','PG-13','R']
-    if sort == 'title'
-      @movies = Movie.all.sort_by { |h | h[:title] }
-      @title_header = ['hilite']
-    elsif sort == 'release_date'
-      @movies = Movie.all.sort_by { |h | h[:release_date] }
-      @release_date_header = ['hilite']
-    end
       
   end
 
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,11 @@
 class Movie < ActiveRecord::Base
+    
+    def self.with_rating_sorting(filters, sort_field)
+        if sort == 'title'
+            @movies = Movie.all.sort_by { |h | h[:title] }
+        elsif sort == 'release_date'
+            @movies = Movie.all.sort_by { |h | h[:release_date] }
+        end
+        @movie.where({:rating => filters})
+    end
 end
