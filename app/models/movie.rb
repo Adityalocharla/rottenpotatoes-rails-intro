6ff583e27a66b1d427c0c8623aa6535254885728
EC2 Default User <ec2-user@ip-172-31-34-248.us-east-2.compute.internal>
2019-02-11 03:10:58 +0000

    par1 test

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,9 +2,9 @@ class Movie < ActiveRecord::Base
     
     def self.with_rating_sorting(filters, sort_field)
         if sort == 'title'
-            self.with_rating_sorting(filters, sort_field).where({:rating => filters}).sort_by { |h | h[:title] }
+            self.where({:rating => filters}).sort_by { |h | h[:title] }
         elsif sort == 'release_date'
-            self.with_rating_sorting(filters, sort_field).where({:rating => filters}).sort_by { |h | h[:release_date] }
+            self.where({:rating => filters}).sort_by { |h | h[:release_date] }
         end
         
     end
